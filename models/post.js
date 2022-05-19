c86e597f7afc981f8933eb8a36b113a0987662ee
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-19 08:11:21 -0500

    defined the model association in the index.js file.

--- models/post.js
+++ models/post.js
@@ -1,6 +1,6 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
-const { post } = require('../routes');
+// const { post } = require('../routes');
 
 // create the post model
 class Post extends Model{};
