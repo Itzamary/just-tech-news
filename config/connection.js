1240499a3442e3ca1d823ccdb7a64941aa928c72
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-24 11:24:16 -0500

    modified the code due to a const mistake causeing errors.

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     // create connection to out database, pass in your mysql information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306
