2cf1e3d2633aad29f8c8837f12156191c0e3a41f
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-25 13:25:00 -0500

    organized the code in the server file.

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const session = require('express-session');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,19 +22,16 @@ const sess = {
   })
 };
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
+const hbs = exphbs.create({});
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
